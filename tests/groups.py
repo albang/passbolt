42ee9c357ea86543262581eecb582aa916eb165a
albang <alban@garrigue.me>
2023-09-03 00:19:33 +0200

    test du sharing de password

--- tests/groups.py
+++ tests/groups.py
@@ -11,12 +11,10 @@ Passbolt = passbolt(privatekey=key, passphrase=passphrase, apiurl=uri)
 class TestPasswordMethods(unittest.TestCase):
     def setUp(self) -> None:
         user = Passbolt.createuser("user@example.com", "Test", "Ing")
-        print(Passbolt.getuser("user@example.com").userid)#
         user2 = Passbolt.createuser("user2@example.com", "Test", "Ing")
 
     def tearDown(self) -> None:
         Passbolt.deleteuser("user@example.com")
-        #Passbolt.deleteuser("user2@example.com")
 
     def test_0_creategroup(self):
         group = Passbolt.creategroup("pytest", ["alban@garrigue.me"], [])
